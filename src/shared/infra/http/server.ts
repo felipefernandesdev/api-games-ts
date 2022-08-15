62b13c90ac01c207bc3dd9fa92472040c5e26308
Felipe Sousa Developer <fesousalocutor@gmail.com>
2022-08-15 17:39:16 -0300

    feat: add middleware error

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
-import express from 'express';
+import { CelebrateError } from 'celebrate';
+import express, { NextFunction, Request, Response } from 'express';
 
 import { router } from './routes';
 
@@ -11,6 +12,16 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(router);
 
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof CelebrateError) {
+    return res.status(400).json({ message: error });
+  }
+
+  res.status(500).json({
+    status: 'Error',
+    message: 'Internal Server Error'
+  });
+});
 app.listen(port, () => {
   console.log(`🚀 API Game listening on port 👉 ${port} and process: ${process.pid}`);
 });
